7fd7586a8a14dcedb7b8abf24d30c3f10e673c40
Aadijain012 <aadijain2806@gmail.com>
2026-08-15 09:51:26 +0000

    Guard Laravel cache directory at Render startup

--- docker/render/start.sh
+++ docker/render/start.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env sh
+
 set -eu
 
-mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views
+
+
+mkdir -p bootstrap/cache storage/framework/cache storage/framework/sessions storage/framework/views
+
 chown -R www-data:www-data storage bootstrap/cache
+
 sed "s/__PORT__/${PORT:-10000}/g" /etc/nginx/http.d/default.conf.template > /etc/nginx/http.d/default.conf
 
+
+
 php artisan migrate --force
+
 php artisan db:seed --force
+
 php artisan optimize:clear
 
+
+
 php-fpm -D
+
 exec nginx -g 'daemon off;'
+
